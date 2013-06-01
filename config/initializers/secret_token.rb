ad02c045514175adba092634a92617664cd6011e
Manuel Reich <Ernesto@Manu.local>
2013-06-01 15:43:04 +0200

    created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbMukke::Application.config.secret_token = 'd728f7c359cad118f07a3c0092a4f6058d9b7a7d80c6396a90b5d33b3c47d285c5f4b9fd9dae5b99ef2cdff407a3a7ba68784b877c0547c9a7a539c5820c7864'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FbMukke::Application.config.session_store :cookie_store, key: '_fb-mukke_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FbMukke::Application.config.session_store :active_record_store
