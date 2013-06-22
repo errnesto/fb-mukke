94daff45528ad6315cf95c700ec17f985de22953
Manuel Reich <errnesto@me.com>
2013-06-22 21:51:26 +0200

    fix was too stupid

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ FbMukke::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index', :via => :get
-  root :to => redirect('/foo.html'), :via => :post
   
   get "home/index"
   get "song/star"
