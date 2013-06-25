d9569e84bad220d6d36e6ea5d56f5b211db6153e
Manuel Reich <errnesto@me.com>
2013-06-25 14:02:51 +0200

    rewrote tests for song model

--- test/models/song_test.rb
+++ test/models/song_test.rb
@@ -1,65 +1,55 @@
 require "test_helper"
 
 describe Song do
-	before do
-		@song = Song.new
-	end
 
 	it "can tell when a youtube link is a song" do
-		@song.url = 'http://www.youtube.com/watch?v=9PnOG67flRA'
-		@song.isSong?.must_equal true
+		songHash = Song.fetch_atributes('http://www.youtube.com/watch?v=9PnOG67flRA')
+		songHash.wont_be_nil
 	end
 	it "can tell that a soundcloud link is a song" do
-		@song.url = 'https://soundcloud.com/fineripp/fineripps-downbeat-menu'
-		@song.isSong?.must_equal true
+		songHash = Song.fetch_atributes('https://soundcloud.com/fineripp/fineripps-downbeat-menu')
+		songHash.wont_be_nil
 	end
 	it "can tell when a youtube link is not a song" do
-		@song.url ='http://www.youtube.com/watch?v=0fKBhvDjuy0'
-		@song.isSong?.must_equal false
+		songHash = Song.fetch_atributes('http://www.youtube.com/watch?v=0fKBhvDjuy0')
+		songHash.must_be_nil
+	end
+
+	it "can tell that a any other link is not a song" do
+		songHash = Song.fetch_atributes('http://www.google.com')
+		songHash.must_be_nil
 	end
 
 	it "can find out the source of its soundcloud url" do
-		@song.url = 'https://soundcloud.com/fineripp/fineripps-downbeat-menu'
-		@song.isFrom.must_equal 'soundcloud'
+		songHash = Song.fetch_atributes('https://soundcloud.com/fineripp/fineripps-downbeat-menu')
+		songHash['source'].must_equal 'soundcloud'
 	end
 
 	it "can find out the source of its youtube url" do
-		@song.url = 'http://www.youtube.com/watch?v=9PnOG67flRA'
-		@song.isFrom.must_equal 'youtube'
+		songHash = Song.fetch_atributes('http://www.youtube.com/watch?v=9PnOG67flRA')
+		songHash['source'].must_equal 'youtube'
 	end
 
 	describe "metadata of the song" do
 
 		it "can find out its name from its youtube url" do
-			@song.url = 'http://www.youtube.com/watch?v=9PnOG67flRA'
-			if (@song.isSong?)
-				@song.set_atributes
-			end
-			@song.name.must_equal 'Metronomy - The Bay'
+			songHash = Song.fetch_atributes('http://www.youtube.com/watch?v=9PnOG67flRA')
+			songHash['name'].must_equal 'Metronomy - The Bay'
 		end
 
 		it "can find out its name from its soundcloud url" do
-			@song.url = 'https://soundcloud.com/fineripp/fineripps-downbeat-menu'
-			if (@song.isSong?)
-				@song.set_atributes
-			end
-			@song.name.must_equal "FineRipp's Downbeat Menu"
+			songHash = Song.fetch_atributes('https://soundcloud.com/fineripp/fineripps-downbeat-menu')
+			songHash['name'].must_equal "FineRipp's Downbeat Menu"
 		end
 
 		it "can get an image from its youtube url" do
-			@song.url = 'http://www.youtube.com/watch?v=9PnOG67flRA'
-			if (@song.isSong?)
-				@song.set_atributes
-			end
-			@song.image.must_equal 'https://i.ytimg.com/vi/9PnOG67flRA/mqdefault.jpg'
+			songHash = Song.fetch_atributes('http://www.youtube.com/watch?v=9PnOG67flRA')
+			songHash['image'].must_include 'ytimg.com/vi/9PnOG67flRA/mqdefault.jpg'
 		end
 
 		it "can get an image from its soundcloud url" do
-			@song.url = 'https://soundcloud.com/fineripp/fineripps-downbeat-menu'
-			if (@song.isSong?)
-				@song.set_atributes
-			end
-			@song.image.must_equal 'http://i1.sndcdn.com/artworks-000046589747-3ubydb-t300x300.jpg?0c5f27c'
+			songHash = Song.fetch_atributes('https://soundcloud.com/fineripp/fineripps-downbeat-menu')
+			songHash['image'].must_include 'sndcdn.com/artworks-000046589747-3ubydb-t300x300.jpg'
 		end
 
 	end
