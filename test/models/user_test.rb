53fdc6aea5127d087bd463c39e7e47dd399ba24f
Manuel Reich <errnesto@me.com>
2013-06-25 14:03:19 +0200

    rewrote tests for song model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,7 +8,7 @@ describe User do
 
 	it "stores user information" do
 
-		@user.uid.must_be :==, '100003562633226'
+		@user.uid.must_be :==, 100003562633226
 		@user.first_name.must_be :==, 'Wunderundfitzig'
 		@user.oauth_token.must_be :==, 'CAAHONCAfHIIBAJRLmroOYudfZCWRDnKktde73YYNNCooHZCMqHfuTvi6sylmKOPbgseXQZBSAprXz7w2yxFB9ZA36elbR0RgOggp741rZBCeMn8Er1dMI4NmZCaymVJC22bc8UcT1KlIKJ2LZBdy3SISvJqbU1wFREZD'
 		@user.oauth_expires_at.must_be :==, '2011-11-20 01:00:05'
